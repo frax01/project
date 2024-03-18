093771d7e697eefa5062a1c3e37d0dccd966ab79
kiinami <kinami@posteo.net>
2024-03-18 15:12:38 +0100

    feat: desktop details page

--- lib/pages/club/programPage.dart
+++ lib/pages/club/programPage.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_layout/adaptive_layout.dart';
 import 'package:flutter/material.dart';
 
 class ProgramScreen extends StatelessWidget {
@@ -11,8 +12,6 @@ class ProgramScreen extends StatelessWidget {
   final Map weather;
 
   Widget weatherTile(Map weather) {
-    print("weather: $weather");
-    print("check: ${weather["check"]}");
     if (weather["check"] == "true" || weather["check"]) {
       return ListTile(
         leading: Image.network(weather["image"], width: 50, height: 50),
@@ -26,8 +25,7 @@ class ProgramScreen extends StatelessWidget {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget smallScreen() {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -158,6 +156,157 @@ class ProgramScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget bigScreen() {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    SizedBox(
+                      height: double.infinity,
+                      child: Card(
+                        clipBehavior: Clip.antiAlias,
+                        elevation: 5,
+                        child: Image.network(
+                          document['imagePath'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 30,
+                      right: -15,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black54,
+                              spreadRadius: 2,
+                              blurRadius: 15,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                          child: Text(
+                            document['title'],
+                            style: const TextStyle(
+                              fontSize: 45,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 20, 20, 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Chip(
+                            avatar: document['selectedOption'] == 'weekend'
+                                ? const Icon(Icons.calendar_today)
+                                : document['selectedOption'] == 'trip'
+                                    ? const Icon(Icons.holiday_village)
+                                    : const Icon(Icons.star),
+                            label: Text(document['selectedOption']
+                                .toString()
+                                .toUpperCase()),
+                            labelStyle: const TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                            labelPadding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Chip(
+                            label: Text(document['selectedClass']
+                                .toString()
+                                .toUpperCase()),
+                            labelStyle: const TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                            labelPadding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20.0),
+                      const Text(
+                        'Descrizione',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        document['description'],
+                        style: const TextStyle(fontSize: 17),
+                      ),
+                      const SizedBox(height: 20.0),
+                      const Text(
+                        'Quando',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        document['endDate'].isNotEmpty
+                            ? '${document['startDate']} ~ ${document['endDate']}'
+                            : document['startDate'],
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(height: 20.0),
+                      const Text(
+                        'Dove',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        document['address'],
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      weatherTile(weather),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+      smallLayout: smallScreen(),
+      largeLayout: bigScreen(),
     );
   }
 }
